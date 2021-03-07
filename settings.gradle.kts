311c0297d24da29caf1588fe4320098b550f7207
Matthew Olsson <matthewcolsson@gmail.com>
2021-03-07 11:18:38 -0800

    Only support 1.8.9 and 1.16.5

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,10 +23,8 @@ rootProject.buildFileName = "root.gradle.kts"
 
 listOf(
     "1.8.9",
-    "1.12.2",
-    "1.15.2",
-    "1.16.2"
-//    "1.16.2-fabric"
+    "1.16.5"
+//    "1.16.5-fabric"
 ).forEach { version ->
     include(":$version")
     project(":$version").apply {
