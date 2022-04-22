82bcad3f676cccc55ff5fe312057deeb5bc6e0b9
Cam Walter <cam.n.walter@gmail.com>
2022-04-22 18:10:01 -0500

    ProvidedLibs: Switch InteractAction to CT's interact action enum

--- src/main/kotlin/com/chattriggers/ctjs/minecraft/listeners/ClientListener.kt
+++ src/main/kotlin/com/chattriggers/ctjs/minecraft/listeners/ClientListener.kt
@@ -325,7 +325,7 @@ object ClientListener {
     }
 
     /**
-     * Used as a pass through argument in [com.chattriggers.ctjs.engine.IRegister.registerPlayerInteract].\n
+     * Used as a pass through argument in [com.chattriggers.ctjs.engine.IRegister.registerPlayerInteract].
      * Exposed in providedLibs as InteractAction.
      */
     enum class PlayerInteractAction {
